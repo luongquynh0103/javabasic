70de19a58d5a53d2618fbdfba5020949bc1f5439
luongquynh0103 <luongquynh0103@gmail.com>
2018-12-23 11:00:02 +0700

    jirstJava

---
+++ jirstJava.java
@@ -0,0 +1,15 @@
+package javabasic;
+
+public class jirstJava {
+	public static void main(String[] args) {
+		int a=0, b=0, c=0;
+		
+		a= 10;
+		
+		b= 10/5;
+		
+		c= b=a;
+		
+		System.out.println(c);			
+	}
+}
